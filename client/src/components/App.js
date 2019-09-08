576c4ae1d4eb446ff4701537c345fbe1a0d73e74
Kyle Schafer <kschay@protonmail.com>
2019-09-08 11:04:55 -0500

    Closes #2 - Created routes for stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">
-        <button>To Page Two</button>
-      </Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">
-        <button>To Page One</button>
-      </Link>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
