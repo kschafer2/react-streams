6eef6dd20505a3489b166a0ae04b374444a6dd15
Kyle Schafer <kschay@protonmail.com>
2019-09-08 10:53:44 -0500

    Closes #1 - Created all stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">
+        <button>To Page Two</button>
+      </Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <Link to="/">
+        <button>To Page One</button>
+      </Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
