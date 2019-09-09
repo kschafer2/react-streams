6940389cdf033d3cdd44e59f3c798875277adb26
Kyle Schafer <kschay@protonmail.com>
2019-09-09 10:54:26 -0500

    Closes #5 - Implemented user auth status

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,18 +3,38 @@ import React from 'react';
 // Google API Client Docs
 // https://developers.google.com/identity/sign-in/web/reference
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     //available on window scope
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
           clientId: '779720330212-i9s8bq8582vt922bb3btj9vpchtn4non.apps.googleusercontent.com',
           scope: 'email'
+        }).then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return <div>I dont know if I am signed in</div>;
+    } else if(this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
